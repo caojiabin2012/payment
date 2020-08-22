224478fbe3f1b6b486e54514aedd0d9fbbcfabf6
cao.jiabin <caojiabin2012@126.com>
2020-08-22 16:48:41 +0800

    test

---
+++ src/Doper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caojiabin
+ * Date: 2020/8/22
+ * Time: 4:41 PM
+ */
+
+namespace Payment;
+
+
+class Doper
+{
+    public function show()
+    {
+        return "doper show";
+    }
+
+    public function say()
+    {
+        return "doper say";
+    }
+}
---
+++ src/Test.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caojiabin
+ * Date: 2020/8/22
+ * Time: 4:41 PM
+ */
+
+namespace Payment;
+
+
+class Test
+{
+    public function show()
+    {
+        return "show";
+    }
+
+    public function say()
+    {
+        return "say";
+    }
+}
